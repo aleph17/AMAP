7202e6af699b8df648a76b5598bca00fa19134cb
aleph17 <muhammadalisayfiddinov@gmail.com>
2024-12-24 17:52:30 +0100

    minor error

--- test.py
+++
@@ -1,6 +0,0 @@
-import cv2 as cv
-
-img = cv.imread('/home/muhammad-ali/github/AMAP/data/test_results/2_result.png')
-img = cv.resize(img, (1024,1024))
-
-cv.imwrite('/home/muhammad-ali/Downloads/r.jpg', img)
